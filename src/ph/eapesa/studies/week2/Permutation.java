995e44ef077d1b0aaa7f61c3cb5e5c7bf7e4f28d
Elixa-Mae Pesa <eapesa@gmail.com>
2018-05-27 19:54:53 +0800

    Added solution to Permutation assignment

--- src/ph/eapesa/studies/week2/Permutation.java
+++ src/ph/eapesa/studies/week2/Permutation.java
@@ -1,7 +1,23 @@
 package ph.eapesa.studies.week2;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Invalid arguments");
+            return;
+        }
 
+        RandomizedQueue<String> inputs = new RandomizedQueue<>();
+        int k = Integer.parseInt(args[0]);
+        while (!StdIn.isEmpty()) {
+            inputs.enqueue(StdIn.readString());
+        }
+
+        for (int i = 0; i < k; i++) {
+            StdOut.println(inputs.dequeue());
+        }
     }
 }
